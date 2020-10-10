e3a3e96c55e928de46ff63d3c0cb0747e940d591
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-10-09 22:36:32 -0300

    Initial commit

---
+++ app/src/main/java/br/com/lucolimac/toolbar/MainActivity.kt
@@ -0,0 +1,13 @@
+package br.com.lucolimac.toolbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(tolbar)
+    }
+}
