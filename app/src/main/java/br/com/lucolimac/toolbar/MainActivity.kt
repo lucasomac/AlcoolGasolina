b246a2a83db0a51e707d56a6fbbbb9c1c07d0772
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-10-12 10:53:10 -0300

    Finalizaçãoda lógica. App finalizado e funcionado.

--- app/src/main/java/br/com/lucolimac/toolbar/MainActivity.kt
+++ app/src/main/java/br/com/lucolimac/toolbar/MainActivity.kt
@@ -1,13 +1,20 @@
 package br.com.lucolimac.toolbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Double.parseDouble
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(tolbar)
+        btComparar.setOnClickListener {
+            if (parseDouble(etanolEdit.text.toString()) / parseDouble(gasolinaEdit.text.toString()) <= 0.7)
+                imageResult.setImageResource(R.drawable.abasteca_etanol)
+            else
+                imageResult.setImageResource(R.drawable.abasteca_gasolina)
+        }
     }
 }
